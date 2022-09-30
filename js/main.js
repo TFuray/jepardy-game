aee1a742a73f53ac41e3c49e6cc087fedd1865cc
TFuray <pk.master881@gmail.com>
2022-09-30 16:29:30 -0700

    added function to create board

--- js/main.js
+++ js/main.js
@@ -1,1 +1,37 @@
+// initialize game board on load
+initBoard()
+initCatRow()
 
+function initCatRow(){
+    let catRow = document.getElementById('category-row')
+        for (let j=0; j<6; j++){
+            let box = document.createElement('div')
+            box.className = 'clue-box category-box'
+            catRow.appendChild(box)
+        }
+
+}
+
+function initBoard() {
+   let board = document.getElementById('clue-board')
+   // generate 5 rows, then place 6 boxes in each row
+
+   for (let i = 0; i < 5; i++){
+        let row = document.createElement('div')
+        let boxValue = 200 * (i + 1)
+        row.className = 'clue-row'
+
+        for (let j=0; j<6; j++){
+            let box = document.createElement('div')
+            box.className = 'clue-box'
+            box.textContent = `$` + boxValue
+            box.addEventListener('click', getClue, false)
+            row.appendChild(box)
+        }
+        board.appendChild(row)
+    }
+}
+
+function getClue(){
+    return 0
+}
