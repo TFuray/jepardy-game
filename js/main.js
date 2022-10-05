fa7abc3e153a7636aba59b1f12ed628bd4ccc48c
TFuray <pk.master881@gmail.com>
2022-10-05 14:54:25 -0700

    added fetch categories

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
-// initialize game board on load
+// initialize game board on load\
+
+// http://jservice.io/api/categories?&count=30
+
 initBoard()
 initCatRow()
-
+buildCategories()
+document.querySelector('button').addEventListener('click', startGame)
 function initCatRow(){
     let catRow = document.getElementById('category-row')
         for (let j=0; j<6; j++){
@@ -32,6 +36,43 @@ function initBoard() {
     }
 }
 
+function randInt(){
+    return Math.floor(Math.random() * (18418) + 1)
+}
+
+let catArray = []
+
+function buildCategories(){
+    const fetchReq1 = fetch(
+        `https://jservice.io/api/category?&id=${randInt()}`).then((res) => res.json())
+    
+    const fetchReq2 = fetch(
+        `https://jservice.io/api/category?&id=${randInt()}`).then((res) => res.json())
+
+    const fetchReq3 = fetch(
+        `https://jservice.io/api/category?&id=${randInt()}`).then((res) => res.json())
+
+    const fetchReq4 = fetch(
+        `https://jservice.io/api/category?&id=${randInt()}`).then((res) => res.json())
+
+    const fetchReq5 = fetch(
+        `https://jservice.io/api/category?&id=${randInt()}`).then((res) => res.json())
+
+    const fetchReq6 = fetch(
+        `https://jservice.io/api/category?&id=${randInt()}`).then((res) => res.json())
+    
+    const allData = Promise.all([fetchReq1,fetchReq2,fetchReq3,fetchReq4,fetchReq5,fetchReq6])
+    
+    allData.then((res) => {
+        console.log(res)
+        catArray = res
+    })
+}
+
 function getClue(){
     return 0
+}
+
+function startGame() {
+
 }
